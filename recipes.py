7de713d3eecad3f29f479cfc07893fde20fd524f
Шолдаев Александр <alivsholdaev@edu.hse.ru>
2026-06-02 23:26:03 +0300

    Создан и доработан класс Recipe

--- recipes.py
+++ recipes.py
@@ -12,7 +12,7 @@ class Ingredient:
     def quantity(self, value: float) -> None:
         value = float(value)
         if value <= 0:
-            raise ValueError("Количество не может быть отрицательным")
+            raise ValueError("Количество должно быть положительным")
         self._quantity = value
 
     def __repr__(self) -> str:
@@ -25,3 +25,39 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return NotImplemented
         return self.name == other.name and self.unit == other.unit
+
+class Recipe:
+    def __init__(self, title: str, ingredients: list[Ingredient] | None = None):
+        self.title = title
+        self.ingredients = []
+        if ingredients is not None:
+            for ingredient in ingredients:
+                self.add_ingredient(ingredient)
+
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        for reality_ingredient in self.ingredients:
+            if reality_ingredient == ingredient:
+                reality_ingredient.quantity += ingredient.quantity
+                return
+        self.ingredients.append(
+            Ingredient(ingredient.name, ingredient.quantity, ingredient.unit)
+        )
+
+    @staticmethod
+    def is_valid_ratio(ratio) -> bool:
+        return isinstance(ratio, Real) and ratio > 0
+
+    def scale(self, ratio: float) -> "Recipe":
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэффициент должен быть положительным")
+        scaled_ingredients = [Ingredient(ingredient.name, ingredient.quantity * ratio,ingredient.unit,) for ingredient in self.ingredients]
+        return Recipe(self.title, scaled_ingredients)
+
+    def __len__(self) -> int:
+        return len(self.ingredients)
+
+    def __str__(self) -> str:
+        ingredients_text = "\n".join(str(ingredient) for ingredient in self.ingredients)
+        if not ingredients_text:
+            return self.title
+        return f"{self.title}\n{ingredients_text}"
